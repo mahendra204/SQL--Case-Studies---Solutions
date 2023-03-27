747155d8addecd961719092813b49bbe43b4942a
Mahendra Kamireddy <120042495+mahendra204@users.noreply.github.com>
2023-03-27 22:24:31 +0530

    Add files via upload

--- Danny's Dinner Case Study/TableScript & Solutions to case study Qs.sql
+++ Danny's Dinner Case Study/TableScript & Solutions to case study Qs.sql
@@ -29,7 +29,7 @@ select * from menu
 select * from sales
 select * from members
 
-
+drop table members
 --problems and solutions--
 --Q1: what is the total amount spent by each customer at restaurent?
 select s.customer_id, sum(price) as total_amount_spent from menu m join sales s on m.product_id = s.product_id group by customer_id
@@ -56,4 +56,46 @@ select customer_id, product_name,count(s.product_id) as no_of_times, DENSE_RANK(
 count(s.product_id) desc) as rnk from menu m join sales s on s.product_id = m.product_id group by customer_id,product_name) 
 select customer_id,product_name,no_of_times from cte where rnk=1
 
+ --Q6: which item was purchased first by a customer after they became a member.
+
+with cte as(
+select m.product_name,s.order_date,s.customer_id,v.join_date,dense_rank() over(partition by s.customer_id order by order_date) as rn 
+from menu m join sales s on s.product_id = m.product_id join members v on v.customer_id=s.customer_id 
+where order_date>=join_date )
+select customer_id,product_name from cte where rn=1
+
+--Q7: which item was purchased just before customer became a member.
+with cte as(
+select m.product_name,s.order_date,s.customer_id,v.join_date,dense_rank() over(partition by s.customer_id order by order_date desc) as rn 
+from menu m join sales s on s.product_id = m.product_id join members v on v.customer_id=s.customer_id 
+where order_date<join_date )
+select customer_id,product_name from cte where rn=1
+
+--Q8: what is the total items and amount spent by a customer before he became a member.
+
+select s.customer_id, count(s.product_id)as no_of_items_purchased_after_became_member, sum(m.price) as total_amount_spent from menu m 
+join 
+sales s on s.product_id = m.product_id 
+join 
+members v on v.customer_id=s.customer_id where order_date<join_date group by s.customer_id
+
+--Q9: if each $1 spent equates 10 points and shushi has a 2x points multipler. how many points would each customer have?
+select s.customer_id,
+sum(case 
+	when product_name='shushi' then 20*price 
+	else 10*price end) as points from sales s
+join menu m on m.product_id = s.product_id group by customer_id 
+
+--Q10: in a first week after customer joins the program(including their join date) they earn 2x points on all items,
+-- not just sushi, how many points do custome A and B have at the end of january?
+
  
+---join all tables and observing the result and from which we can understand easily.
+with cte as (select customer_id,product_name,order_date,price
+ from menu join 
+sales on sales.product_id = menu.product_id) 
+select * from cte where customer_id in (select customer_id from members)order by order_date,customer_id,product_name
+
+select * from sales 
+select * from menu
+select * from members
